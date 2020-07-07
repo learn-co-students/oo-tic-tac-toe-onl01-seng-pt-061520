891ceb41806270da1283cc327541c15b412c7099
Cameron Hudson <camnn3@yahoo.com>
2020-07-07 18:40:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,77 +1,74 @@
 class TicTacToe
-  def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  def initialize(board = nil) 
+    @board = board || Array.new(9, " ")
   end
-  
+
   WIN_COMBINATIONS = [
-    [0,1,2], [0,3,6], [1,4,7],
-    [3,4,5], [2,5,8], [0,4,8],
-    [6,7,8], [2,4,6]
-    
-    ]
-    
-    def display_board
-      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-      puts "-----------"
-      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-      puts "-----------"
-      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-    end
-    
-    def input_to_index(input)
-      input.to_i - 1
-    end
-    
-    def move(index, token)
-      @board[index.to_i - 1] = token
-    end
-    
-    def position_taken?(position)
-      if @board[position] == "X" || @board[position] == "O"
-        true
-      else
-        false
-      end
-    end
-    
-    def valid_move?(position)
-      position = position.to_i - 1
-      if position.between?(0,8) && !position_taken?(position)
-        true
-      else
-        false
-      end
-    end
-    
-    def turn
-      puts "Please enter 1-9:"
-      input = gets.strip
-      if valid_move?(input)
-        move(input, current_player)
-      else
-        turn
-      end
-      display_board
-    end
-    
-    def turn_count
-      counter = 0
-      @board.each do |i|
-        if i == "X" || i == "O"
-          counter += 1
-        end
-      end
-      counter
-    end
-    
-    def current_player
-      turn_count % 2 == 0 ? "X" : "O"
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+  ]
+
+  def display_board 
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def move(location, character = "X")
+    @board[location.to_i - 1] = character
+  end
+
+  def position_taken?(position)
+    if @board[position] == "X" || @board[position] == "O"
+      true
+    else
+      false
+    end 
+  end
+
+  def valid_move?(position)
+    position = position.to_i - 1
+    if position.between?(0,8) && !position_taken?(position)
+      true
+    else
+      false
     end
   end
-  
+
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    if valid_move?(input)
+      move(input, current_player)
+    else
+      turn
+    end
+    display_board
+  end
+
+  def turn_count
+    counter = 0
+    @board.each do |i|
+      if i == "X" || i == "O"
+        counter += 1
+      end
+    end
+    counter
+  end
+
   def current_player
     turn_count % 2 == 0 ? "X" : "O"
   end
+
+
 
   def won?
 
@@ -123,6 +120,13 @@ end
       puts "Cats Game!"
     end
   end
+
+
+
+
+
+
+end 
   
   
   
